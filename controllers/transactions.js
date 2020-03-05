aa90fe969f8c5a0f97ea200daa821fd2fe59830d
ncux <malabatalent@gmail.com>
2020-03-05 15:09:51 +0800

    ongoing

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,23 +1,38 @@
+const Transaction = require('../models/Transaction');
 
 // GET route to fetch all transactions
 // route: /api/v1/transactions
-exports.getTransactions = (req, res, next) => {
-
-    res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
+        return res.status(200).json({
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (e) {
+        res.status(500).json({
+            error: `Server error; ${ e.message }`
+        });
+    }
     next();
 };
 
 // POST route to add a transaction
 // route: /api/v1/transactions
-exports.addTransaction = (req, res, next) => {
-
-    res.send('POST transaction');
+exports.addTransaction = async (req, res, next) => {
+    try {
+        const { text, amount } = req.body;
+        const transaction = await Transaction.create(req.body);
+        return res.status(201).json({ transaction });
+    } catch (e) {
+        console.log(e);
+    }
     next();
 };
 
 // route to DELETE a transaction
 // route: /api/v1/transactions/:id
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
 
     res.send('DELETE transaction');
     next();
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const path = require('path');
 
+dotenv.config({ path: './config/config.env' });
+
+const port = process.env.PORT || 5000;
+
+const database = require('./config/database');
+database();
+
 // routes
 const transactions = require('./routes/transactions');
 
-dotenv.config({ path: './config/config.env' });
-const port = process.env.PORT || 5000;
-
 const app  = express();
+
+// body-parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hi!'));
 app.use('/api/v1/transactions', transactions);
