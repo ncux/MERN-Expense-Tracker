28b638a0170ffe7fd8fa5f849c0dd7b2bf1cb367
ncux <malabatalent@gmail.com>
2020-03-05 17:27:26 +0800

    pending client stuff

--- client/src/contexts/transactions/context.js
+++ client/src/contexts/transactions/context.js
@@ -1,8 +1,14 @@
 import React, { createContext, useReducer } from 'react';
 import { Reducer } from "./reducer";
-import { ADD_TRANSACTION, DELETE_TRANSACTION } from "../types";
+import { ADD_TRANSACTION, GET_TRANSACTIONS, DELETE_TRANSACTION, GET_TRANSACTIONS_ERROR } from "../types";
+
+import axios from 'axios';
+
+const transactions_API = `/api/v1/transactions`;
 
 const TransactionsState = {
+    error: null,
+    loading: true,
     transactions: [
         // { id: 1, text: 'Flower', amount: -20 },
         // { id: 2, text: 'Salary', amount: 300 },
@@ -19,6 +25,16 @@ export const TransactionState = ({ children }) => {
 
     const amounts = state.transactions.map(transaction => transaction.amount);
 
+    const getTransactions = async () => {
+        try {
+            const res = await axios.get(transactions_API);
+            dispatch({ type: GET_TRANSACTIONS, payload: res.data.data });
+        } catch (e) {
+            dispatch({ type: GET_TRANSACTIONS_ERROR, payload: e.response.data.error });
+        }
+
+    };
+
     const deleteTransaction = id => dispatch({ type: DELETE_TRANSACTION, payload: id });
 
     const addTransaction = transaction => dispatch({ type: ADD_TRANSACTION, payload: transaction });
@@ -26,7 +42,10 @@ export const TransactionState = ({ children }) => {
     return (
         <TransactionsContext.Provider value={{
             transactions: state.transactions,
+            loading: state.loading,
+            error: state.error,
             amounts,
+            getTransactions,
             deleteTransaction,
             addTransaction
         }}>
--- client/src/contexts/transactions/reducer.js
+++ client/src/contexts/transactions/reducer.js
@@ -1,8 +1,14 @@
-import { ADD_TRANSACTION, DELETE_TRANSACTION } from "../types";
+import { ADD_TRANSACTION, GET_TRANSACTIONS, GET_TRANSACTIONS_ERROR, DELETE_TRANSACTION } from "../types";
 
 export const Reducer = (state, action) => {
 
     switch (action.type) {
+
+        case GET_TRANSACTIONS:
+            return { ...state, transactions: action.payload, loading: false };
+
+        case GET_TRANSACTIONS_ERROR:
+            return { ...state, error: action.payload, loading: false };
 
         case ADD_TRANSACTION:
             return { ...state, transactions: [action.payload, ...state.transactions] };
